b0ed1d7e7f2a3b75d9fb2affbe587ae4fa45ce5c
HunnicGomes <simon_cheung1974@hotmail.com>
2023-05-05 14:09:33 +0900

    Cleaned up

--- app/src/main/java/org/sorakun/soradisplay/ClockActivity.kt
+++ app/src/main/java/org/sorakun/soradisplay/ClockActivity.kt
@@ -8,10 +8,13 @@ import android.os.Looper
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowInsets
+import android.widget.DigitalClock
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.sorakun.soradisplay.databinding.ActivityClockBinding
+import java.util.*
+
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -20,14 +23,22 @@ import org.sorakun.soradisplay.databinding.ActivityClockBinding
 class ClockActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityClockBinding
-    private lateinit var fullscreenContent: TextView
+    private lateinit var fullscreenContent: SoraDigitalClock
     private lateinit var fullscreenContentControls: LinearLayout
+    private lateinit var dateDisplay: TextView
+
     private val hideHandler = Handler(Looper.myLooper()!!)
 
+    private val dayChangedBroadcastReceiver = object : DayChangedBroadcastReceiver() {
+
+        override fun onDayChanged(format: String) {
+            dateDisplay.text = format
+        }
+    }
     @SuppressLint("InlinedApi")
     private val hidePart2Runnable = Runnable {
         // Delayed removal of status and navigation bar
-        /*if (Build.VERSION.SDK_INT >= 30) {
+        if (Build.VERSION.SDK_INT >= 30) {
             fullscreenContent.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
         } else {
             // Note that some of these constants are new as of API 16 (Jelly Bean)
@@ -40,7 +51,7 @@ class ClockActivity : AppCompatActivity() {
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        }*/
+        }
     }
     private val showPart2Runnable = Runnable {
         // Delayed display of UI elements
@@ -80,8 +91,10 @@ class ClockActivity : AppCompatActivity() {
         isFullscreen = true
 
         // Set up the user interaction to manually show or hide the system UI.
-        //fullscreenContent = binding.fullscreenContent
+        fullscreenContent = binding.simpleDigitalClock
         //fullscreenContent.setOnClickListener { toggle() }
+
+        dateDisplay = binding.textDateDisplay
 
         //fullscreenContentControls = binding.fullscreenContentControls
 
@@ -90,9 +103,18 @@ class ClockActivity : AppCompatActivity() {
         // while interacting with the UI.
         //binding.dummyButton.setOnTouchListener(delayHideTouchListener)
 
-        val UI_OPTIONS =
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or View.SYSTEM_UI_FLAG_VISIBLE
+        // Hide the OS status bar for Jellybean
+        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or View.SYSTEM_UI_FLAG_VISIBLE
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        this.registerReceiver(dayChangedBroadcastReceiver, DayChangedBroadcastReceiver.getIntentFilter())
+
+
+        dateDisplay.text = dayChangedBroadcastReceiver.printDate(Date())
+
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/org/sorakun/soradisplay/DayChangedBroadcastReceiver.kt
@@ -0,0 +1,49 @@
+package org.sorakun.soradisplay
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import java.text.SimpleDateFormat
+import java.util.*
+
+abstract class DayChangedBroadcastReceiver : BroadcastReceiver() {
+
+    private var date = Date()
+    private val dateFormat by lazy { SimpleDateFormat("EE MMM dd", Locale.getDefault()) }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+
+        val currentDate = Date()
+
+        //if ((action == Intent.ACTION_TIME_CHANGED || action == Intent.ACTION_DATE_CHANGED || action == Intent.ACTION_TIMEZONE_CHANGED) && !isSameDay(currentDate)) {
+        if (action == Intent.ACTION_TIME_TICK) {
+            onDayChanged(printDate(currentDate))
+        }
+    }
+
+    private fun isSameDay(currentDate: Date) = dateFormat.format(currentDate) == dateFormat.format(date)
+
+    fun printDate(toBeDisplayed: Date) : String {
+        date = toBeDisplayed
+        return dateFormat.format(toBeDisplayed)
+    }
+
+    abstract fun onDayChanged(format: String)
+
+    companion object {
+
+        /**
+         * Create the [IntentFilter] for the [DayChangedBroadcastReceiver].
+         *
+         * @return The [IntentFilter]
+         */
+        fun getIntentFilter() = IntentFilter().apply {
+            addAction(Intent.ACTION_TIME_TICK)
+            addAction(Intent.ACTION_TIMEZONE_CHANGED)
+            addAction(Intent.ACTION_TIME_CHANGED)
+            addAction(Intent.ACTION_DATE_CHANGED)
+        }
+    }
+}
